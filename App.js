3e56c9cd40a6d3d07250b51f8503ba65f17b2f86
caioxis <caio.joaquim271@gmail.com>
2026-06-08 20:06:15 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
-import HomeScreen from './src/screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import TabNavigator from './src/navigation/TabNavigator';
+import { ReportsProvider } from './src/context/ReportsContext';
 
 export default function App() {
-  return <HomeScreen />;
+  return (
+    <ReportsProvider>
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
+    </ReportsProvider>
+  );
 }
